c9b9669be460146c8a3020c6e6c2d5ea0fa86cf4
ryan <rle71960@ucmo.edu>
2016-06-07 18:32:41 -0500

    Added comment

--- grader.c
+++ grader.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 #define BUFF_SIZE 1024
 
@@ -35,5 +36,39 @@ int main()
 	/*fprintf(stdout, "String read: %s\n", buff);*/
 
 	fclose(file);
+
+	/*
+	 * String splitting taken from
+	 * stackoverflow.com/questions/11198604/c-split-string-into-an-array-of-strings
+	 */
+
+	char ** array = NULL;
+	char * p = strtok(buff, ","); /* split string on ',' */
+	int n_spaces = 0, i;
+
+	while (p)
+	{
+		array = realloc(array, sizeof (char *) * ++n_spaces);
+
+		if (array == NULL)
+		{
+			exit(1); /* memory allocation failed */
+		}
+
+		array[n_spaces-1] = p;
+
+		p = strtok(NULL, ",");
+	}
+
+	array = realloc(array,sizeof (char*) * (n_spaces+1));
+	array[n_spaces] = 0;
+
+	for( i = 0; i < (n_spaces); ++i)
+	{
+		fprintf(stdout, "%d=%s\n", i, array[i]);
+	}
+
+	free(array);
+
 	return 0;
 }
