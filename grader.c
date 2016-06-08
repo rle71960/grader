6433a625f24b5b6bc219f2a90e42dc215f85bcd5
ryan <rle71960@ucmo.edu>
2016-06-07 19:12:22 -0500

    Adds routine to convert str arr to int arr

--- grader.c
+++ grader.c
@@ -7,19 +7,20 @@
 
 #define BUFF_SIZE 1024
 
-float avg;
-float min;
-float max;
-float med;
-int passed;
 
 void *runner(void *param);
 
 int main()
 {
-	pthread_t tid; /* thread */
+	pthread_t average; /* thread */
+	pthread_t minimum;
+	pthread_t maximum;
+	pthread_t median;
+	pthread_t numPassed;
+
 	pthread_attr_t attr; /* thread attributes */
 	
+	/* Read the file, load grades into str array */
 	FILE *file;
 	char *mode = "r";
 
@@ -68,7 +69,44 @@ int main()
 		fprintf(stdout, "%d=%s\n", i, array[i]);
 	}
 
-	free(array);
+	int arr[n_spaces];
+	for ( i = 0; i < n_spaces; ++i)
+	{
+		arr[i] = atoi(array[i]);
+	}
 
+	for ( i = 0; i < n_spaces; ++i)
+	{
+		fprintf(stdout, "%d=%d\n", i, arr[i]);
+	}
+
+	/* Array loaded, spawn threads for functions */
+
+	/*
+	 * average = 
+	 * minimum = 
+	 * maximum = 
+	 * median = 
+	 * numPassed = 
+	 */
+
+	/* Hold return values */
+	float avg;
+	float min;
+	float max;
+	float med;
+	int passed;
+/*
+	pthread_join(average, avg);
+	pthread_join(minimum, min);
+	pthread_join(maximum, max);
+	pthread_join(median, med);
+	pthread_join(numPassed, passed);
+*/
+	free(array);
 	return 0;
+}
+
+float averageGrades(char ** arr)
+{
 }
